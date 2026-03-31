cb12a1485d2eb6c983bf55d784656835faf99429
MAURICE-Pierre-Luc <pl.maurice.fr@gmail.com>
2026-03-31 10:52:05 +0200

    fixed the formula for perimeter

--- perimeter.c
+++ perimeter.c
@@ -2,10 +2,11 @@
 #include "perimeter.h"
 #include <math.h>
 
+
 float perimeter_rect(float height, float width) {
     return 2 * (height + width);
 }
 
 float perimeter_circle(float radius){
-	return 4 * M_PI * radius;
+	return 2 * M_PI * radius;
 }
