d0b69df9733e529cd41f307d3911fe64c9be5920
Votre Nom <votre.adresse@mail.com>
2026-03-31 10:21:17 +0200

    oh noes there's an error

--- tests/test_perimeter.c
+++ tests/test_perimeter.c
@@ -10,7 +10,13 @@ void test_perimeter_rectangle() {
     assert(fabs(perimeter_rect(2.0, 8.0) -20.0) < PRECISION);
 }
 
+void test_perimeter_circle(){
+	assert(fabs(perimeter_circle(4.0) - 25.13274) < PRECISION);
+}
+
+
 int main() {
     test_perimeter_rectangle();
+    test_perimeter_circle();
 }
 
